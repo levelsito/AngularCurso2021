4665f7d47668178c555c039d6157057aa269baee
Jose Luis Campos <joselunior@gmail.com>
2022-01-18 18:59:11 +0100

    Reactive Dinamicos: KeyUp, Validators y Form Array

--- src/app/06-formularios-app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/06-formularios-app/reactive/dinamicos/dinamicos.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -11,7 +11,17 @@ export class DinamicosComponent {
 
   miFormulario: FormGroup = this.fb.group({
     nombre: [ , [ Validators.required, Validators.minLength(3) ] ],
+    favoritos: this.fb.array( [
+      [ 'Metal Gear' ],
+      [ 'Death Stranding' ],
+    ], [ Validators.required, Validators.minLength(2) ] )
   })
+
+  nuevoFavorito: FormControl = this.fb.control( '', Validators.required );
+
+  get favoritosArr(){
+    return this.miFormulario.get('favoritos') as FormArray;
+  }
 
   constructor( private fb: FormBuilder ) {}
 
@@ -19,7 +29,17 @@ export class DinamicosComponent {
     return this.miFormulario.controls[campo].errors && this.miFormulario.controls[campo].touched
   }
 
-  guardar() {
+  agregarFavorito(): void{
+    if( this.nuevoFavorito.invalid ){ return; }
+    this.favoritosArr.push( this.fb.control( this.nuevoFavorito.value, Validators.required ) );
+    this.nuevoFavorito.reset();
+  }
+
+  borrar( pos: number ): void{
+    this.favoritosArr.removeAt(pos)
+  }
+
+  guardar(): void {
     if( this.miFormulario.invalid ) {
       this.miFormulario.markAllAsTouched();
       return;
