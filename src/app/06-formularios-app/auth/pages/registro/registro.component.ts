a83bc0bd07f6b6cfe963d3f68b883bb034bf34c2
Jose Luis Campos <joselunior@gmail.com>
2022-01-27 23:09:40 +0100

    validator service

--- src/app/06-formularios-app/auth/pages/registro/registro.component.ts
+++ src/app/06-formularios-app/auth/pages/registro/registro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorService } from 'src/app/shared/validator/validator.service';
 
 @Component({
   selector: 'app-registro',
@@ -8,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [ '', [Validators.required, Validators.pattern( this.vs.nombreApellidoPattern )] ],
+    email: [ '', [Validators.required, Validators.pattern( this.vs.emailPattern )] ],
+    username: [ '', [Validators.required, this.vs.noPuedeSerUsuario ] ]
+  })
+
+  constructor( private fb: FormBuilder,
+               private vs: ValidatorService ) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'Jose Campos',
+      email: 'test1@test.com',
+      username: 'JoseLuis'
+    })
+  }
+
+  campoNoValido( campo: string ) {
+    return this.miFormulario.get(campo)?.invalid && this.miFormulario.get(campo)?.touched
+  }
+
+  submitFormulario() {
+    console.log(this.miFormulario.value);
+    this.miFormulario.markAllAsTouched();
   }
 
 }
